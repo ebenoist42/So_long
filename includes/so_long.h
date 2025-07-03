6d43abe3aad12ab60e0c892f24aace679b4cc7de
Ebenoist <emma.benoist91@gmail.com>
2025-07-03 14:32:53 +0200

    Add files via upload

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: ebenoist <ebenoist@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/26 09:58:44 by ebenoist          #+#    #+#             */
-/*   Updated: 2025/07/01 09:48:46 by ebenoist         ###   ########.fr       */
+/*   Updated: 2025/07/03 12:06:52 by ebenoist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,31 @@
 # define SO_LONG_H
 
 # include "../libft/libft.h"
-# include <fcntl.h>
 # include "../minilibx/mlx.h"
+# include <fcntl.h>
 
 typedef struct s_game
 {
 	char	**map;
 	int		width;
 	int		height;
-	int		px;       // player x
-	int		py;       // player y
-	int		collect;  // collectibles restants
+	int		p;
+	int collect; // collectibles restants
 	int		moves;
 	void	*mlx;
 	void	*win;
-	void	*img[5];  // sol, mur, joueur, C, E
-}	t_game;
+	void *img[5]; // sol, mur, joueur, C, E
+}			t_game;
 
-
-void	ft_arg_error(int ac, char *file);
-void	ft_error_file(void);
-char	**read_file(char *file);
-void	ft_error_malloc(char **tab);
-void	free_map(char **map);
-void	invalid_map(char **map);
-int	check_norm(char **map);
-
+void		ft_arg_error(int ac, char *file);
+void		ft_error_file(void);
+char		**read_file(char *file);
+void		ft_error_malloc(char **tab);
+void		free_map(char **map);
+void		ft_invalid_map(char **map);
+void		ft_check_norm(char **game);
+void		parse_map_char(char **game);
+int			ft_path(map);
 
 # define TILE_SIZE 64
 
